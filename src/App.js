32e185a6cb0c076feabfdb003bf78e5df23595b2
unknown <monstersmysining@gamil.com>
2024-07-10 19:37:04 +0300

    Заменил старый проект на новый React проект

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainPage from './components/MainPage';
 
 function App() {
-  const value = 'World! We meet again.';
-  return <div>Hello {value} </div>;
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/StartOfMyDiploma', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<Footer />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
